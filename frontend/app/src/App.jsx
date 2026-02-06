01f4ae5034bb9c9012c4b049da5bc60e3f608f84
unknown <949leoni@gmail.com>
2026-02-06 05:05:13 -0300

    create a frontend react+vite

---
+++ frontend/app/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+function App() {
+  return (
+    
+   <div className='container'>
+      <header>1</header>
+      <div className='navBar'>2</div>
+      <div className='slidePrincipal'>3</div>
+      <div className='navBar2'>4</div>
+      <div className='slideSecundario'>5</div>
+      <div className='slideTerceiro'>6</div>
+      <div className='slideQuarto'>7</div>
+      <div className='slideQuinto'>8</div>
+      <div className='slideSeixto'>9</div>
+      <div className='slideSetimo'>10</div>
+      <div className='slideOitavo'>11</div>
+      <div className='navBar3'>12</div>
+   </div>
+  )
+}
+export default App
