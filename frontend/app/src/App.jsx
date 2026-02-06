13341ee4a780d1f95d50ecb3d1ec5cfe51addd50
unknown <949leoni@gmail.com>
2026-02-06 15:45:50 -0300

    create a base layou of site

--- frontend/app/src/App.jsx
+++ frontend/app/src/App.jsx
@@ -1,21 +1,19 @@
 import './App.css'
+import Navbar from './components/Navbar'
 function App() {
   return (
-    
-   <div className='container'>
-      <header>1</header>
-      <div className='navBar'>2</div>
-      <div className='slidePrincipal'>3</div>
-      <div className='navBar2'>4</div>
-      <div className='slideSecundario'>5</div>
-      <div className='slideTerceiro'>6</div>
-      <div className='slideQuarto'>7</div>
-      <div className='slideQuinto'>8</div>
-      <div className='slideSeixto'>9</div>
-      <div className='slideSetimo'>10</div>
-      <div className='slideOitavo'>11</div>
-      <div className='navBar3'>12</div>
-   </div>
+    <div className='app'>
+      <nav>
+        <Navbar />
+      </nav>
+      <main>
+
+      </main>
+      <footer>
+
+      </footer>
+
+    </div>
   )
 }
 export default App
