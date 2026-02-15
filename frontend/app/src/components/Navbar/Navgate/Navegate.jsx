7f628b09b198b20872378b3c86909099fe52a760
unknown <949leoni@gmail.com>
2026-02-15 14:00:59 -0300

    finish the navbar

--- frontend/app/src/components/Navbar/Navgate/Navegate.jsx
+++ frontend/app/src/components/Navbar/Navgate/Navegate.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
+import './Navegate.css'
 
 function Navegate() {
   return (
     <div className="navegateContainer">
-        <div className="normalNavegate"></div>
-        <div className="loginNavegate"></div>
+        <div className="normalNavegate">
+            <ul>
+                <li><NavLink to="/home" >Home</NavLink></li>
+                <li><NavLink to="/menu_browse">Browse Menu</NavLink></li>
+                <li><NavLink to="/offers">Special Offers</NavLink></li>
+                <li><NavLink to="/restaurant">Restaurant</NavLink></li>
+                <li><NavLink to="/track_order">Track Order</NavLink></li>
+            </ul>    
+        </div>
+        <div className="loginNavegate">
+            <ul>
+                 <li><NavLink to="/loginSignup" >Login/Signup</NavLink></li>
+            </ul>
+           
+        </div>
     </div>
   )
 }
